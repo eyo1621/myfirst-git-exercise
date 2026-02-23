bef62c01c21c09bdc2eb615d1c9bd508f71b58be
eyobe <eyobetefera68@gmail.com>
2026-02-23 10:16:46 -0800

    jijjirama koo

---
+++ calcu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"enter two values";
+	cin>>x>>y;
+	cout <<"results of Arithem.ops="<<endl;
+	cout<<"sum="<<(x+y)<<endl;
+	cout<<"difference="<<(x-y)<<endl;
+	cout<<"quotient="<<(x/y)<<endl;
+	cout<<"product="<<(x*y)<<endl;
+	cout<<"reminderr="<<(x%y)<<endl;
+	return 0;
+}
