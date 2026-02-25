76b4e204859d437449179c31770974e677cdc4ef
eyobe <eyobetefera68@gmail.com>
2026-02-25 05:05:29 -0800

    updated

---
+++ passowd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout<<"=====================eyobs software====================="<<endl;
+	int password=11234;
+	
+	int x;
+	cout <<"enter your password sir"<<endl;
+	cin>>x;
+
+	if(password==x)
+	  cout<<"welcome yane qonjo";
+	else
+	  cout<<"tasastashal anchi leba hulla ";
+
+	
+	return 0;
+}
